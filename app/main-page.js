3969ac2acadb0a61ab6c3fed4bc06d130bd7d087
Emil Öberg <emil.oberg@monator.com>
2015-09-16 11:31:33 +0200

    Generic JS module "moment.js" installed from NPM.

--- app/main-page.js
+++ app/main-page.js
@@ -1,5 +1,32 @@
+var Observable = require('data/observable');
+var ObservableArray = require('data/observable-array');
+var http = require("http");
+var moment = require('moment');
+
+var bindingContext = new Observable.Observable({
+    title: 'Nice title',
+    myItems: new ObservableArray.ObservableArray([])
+});
+
 function pageLoaded(args) {
     var page = args.object;
-    page.bindingContext = {};
+    page.bindingContext = bindingContext;
+
+    loadReddit();
 }
+
+
+function loadReddit() {
+    http.getJSON("https://www.reddit.com/.json")
+    .then(function (r) {
+        bindingContext.title = 'Reddit loaded';
+
+        r.data.children.map(function(item) {
+            var out = item.data;
+            out.friendlyTime = moment(item.data.created_utc * 1000).fromNow();
+            bindingContext.myItems.push(out);
+        });
+    });
+}
+
 exports.pageLoaded = pageLoaded;
